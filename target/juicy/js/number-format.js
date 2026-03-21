ecb6e16cfb07ec11e3b3bbf5ef402f45b92d2787
Trần Đức Trí <23130347@st.hcmuaf.edu.vn>
2026-03-21 21:17:40 +0700

    Delete target

--- target/juicy/js/number-format.js
+++
@@ -1,8 +0,0 @@
-export function numberFormat() {
-    document.querySelectorAll('.price-format').forEach(pf => {
-        const value = pf.getAttribute('data-value');
-        pf.innerText = new Intl.NumberFormat('vi-VN', {style: 'currency', currency: 'VND'}).format(value);
-    })
-}
-
-document.addEventListener("DOMContentLoaded", numberFormat);
