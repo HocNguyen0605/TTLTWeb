efd2e77f7bf3a43c47dcf6a8212f1b474a9f3f07
Tran Duc Tri <23130347@st.hcmuaf.edu.vn>
2026-03-27 16:55:10 +0700

    fix: sua doGet trong RegisterServlet de tra ve trang login khi go duong dan vao trang ko bi blank

--- src/main/java/controller/RegisterServlet.java
+++ src/main/java/controller/RegisterServlet.java
@@ -17,7 +17,7 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.sendRedirect("login");
     }
 
     @Override
